8071699b3c93bd87ee938aa779a6d378cb96ba38
Bumba <prasenjuetce@gmail.com>
2017-09-09 23:22:37 +0530

    added more magic

--- ArduinoProjects/NeoPixel/basicLighting/basicLighting.ino
+++ ArduinoProjects/NeoPixel/basicLighting/basicLighting.ino
@@ -7,7 +7,9 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NO_LEDS, PIN_IN, NEO_GRB + NEO_KHZ8
 unsigned int ringSingleColorDelay = 100;
 unsigned int ringDoubleColorDelay = 100;
 unsigned int ringTripleColorDelay = 100;
-unsigned int ringSingleColorWithGapDelay = 100;
+unsigned int ringSingleColorWithGapDelay = 200;
+unsigned int ringToggleSequenceDelay = 100;
+
 unsigned int ringBrightness = 50;
 
 uint32_t red = pixels.Color(255, 0, 0);
@@ -17,6 +19,7 @@ uint32_t cyan = pixels.Color(0, 255, 255);
 uint32_t magenta = pixels.Color(255, 0, 255);
 uint32_t yellow = pixels.Color(255, 255, 0);
 uint32_t white = pixels.Color(255, 255, 255);
+uint32_t pink = pixels.Color(255,192,203);
 
 void makeRingSingleColor(int delayVal) {
   pixels.clear();
@@ -24,6 +27,16 @@ void makeRingSingleColor(int delayVal) {
   for(int i = 0; i < NO_LEDS; i++){
     delay(delayVal);
     pixels.setPixelColor(i, magenta);
+    pixels.show();
+  }
+}
+
+void makeRingSingleColorReverse(int delayVal) {
+  pixels.clear();
+  
+  for(int i = NO_LEDS - 1; i >= 0; i--){
+    delay(delayVal);
+    pixels.setPixelColor(i, cyan);
     pixels.show();
   }
 }
@@ -62,14 +75,60 @@ void makeRingTripleColor(int delayVal) {
   }
 }
 
+void makeRingTripleColorReverse(int delayVal) {
+  pixels.clear();
+  
+  for(int i = NO_LEDS - 1; i >= 0; i--){
+    delay(delayVal);
+    
+    if(i % 3 == 0) {
+      pixels.setPixelColor(i, red);
+    } else if(i % 2 == 0) {
+      pixels.setPixelColor(i, green);
+    } else {
+      pixels.setPixelColor(i, blue);
+    }
+    
+    pixels.show();
+  }
+}
+
 void makeRingSingleColorWithGap(int delayVal) {
   pixels.clear();
   
-  for(int i = 0; i < NO_LEDS; i++){
+  for(int i = 0; i < NO_LEDS; i += 2){
     delay(delayVal);
-    if(i % 2 == 0) pixels.setPixelColor(i, cyan);
+    pixels.setPixelColor(i, cyan);
     pixels.show();
   }
+}
+
+void makeRingToggleSequence(int delayVal, int times) {
+  int c = 0;
+  
+  while(c <= times) {
+    c++;
+    delay(delayVal);
+    pixels.clear();
+  
+    for(int i = 0; i < NO_LEDS; i += 2){
+      pixels.setPixelColor(i, red);
+      pixels.show();
+    }
+    
+    delay(delayVal);
+    pixels.clear();
+    
+    for(int i = 1; i < NO_LEDS; i += 2){
+      pixels.setPixelColor(i, blue);
+      pixels.show();
+    }
+  }
+}
+
+void makeRingSequenceDim(int delayVal) {
+  pixels.setPixelColor(0, pixels.Color(255 * sin(millis()),0,0));
+  pixels.show();
 }
 
 void setup() {
@@ -79,7 +138,9 @@ void setup() {
 void loop() {
   pixels.setBrightness(ringBrightness);
   makeRingSingleColor(ringSingleColorDelay);
+  makeRingSingleColorReverse(ringSingleColorDelay);
   makeRingDoubleColor(ringDoubleColorDelay);
-  makeRingTripleColor(ringTripleColorDelay);
+  makeRingTripleColorReverse(ringTripleColorDelay);
   makeRingSingleColorWithGap(ringSingleColorWithGapDelay);
+  makeRingToggleSequence(ringToggleSequenceDelay, 10);
 }
