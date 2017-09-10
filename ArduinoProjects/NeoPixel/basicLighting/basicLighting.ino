20f1f130ad9dd882d9fb14ff3477e362e52d95e3
Bumba <prasenjuetce@gmail.com>
2017-09-10 21:32:32 +0530

    added rainbow

--- ArduinoProjects/NeoPixel/basicLighting/basicLighting.ino
+++ ArduinoProjects/NeoPixel/basicLighting/basicLighting.ino
@@ -241,8 +241,38 @@ void makeRingSingleColorDimSimultaneous(int times) {
   }
 }
 
+//taken from example
+void rainbow() {
+  uint16_t i, j;
+
+  for(j = 0; j < 256; j++) {
+    for(i = 0; i < NO_LEDS; i++) {
+      pixels.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    
+    pixels.show();
+    delay(20);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
 void setup() {
   pixels.begin();
+  pixels.show(); //set all to off
 }
 
 void loop() {
@@ -258,4 +288,5 @@ void loop() {
   makeRingToggleSequence(red, blue, 10);
   makeRingDoubleColorDimIndependent(50);
   makeRingBounceTwoColor(red, blue, 2);
+  rainbow();
 }
